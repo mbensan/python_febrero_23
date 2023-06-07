925a61750d74c4c897080219053448a7bc26c030
Matias Bensan Araya <mbensan@gmail.com>
2023-06-06 20:02:25 -0400

    ABB's

--- proyectos_algoritmos/arboles.py
+++ proyectos_algoritmos/arboles.py
@@ -19,6 +19,46 @@ class Nodo:
                 self.der = Nodo(llave, valor)
             else:
                 self.der.add(llave, valor)
+    
+    def buscar(self, llave):
+        if self.llave == llave:
+            return self.valor
+        
+        elif llave < self.llave:
+            if self.izq is None:
+                return None
+            return self.izq.buscar(llave)
+        
+        else: # llave > self.llave
+            if self.der is None:
+                return None
+            return self.der.buscar(llave)
+        
+    def largo(self):
+        if self.izq is None and self.der is None:
+            return 1
+        
+        elif self.izq is None:
+            return 1 + self.der.largo()
+
+        elif self.der is None:
+            return 1 + self.izq.largo()
+
+        else:
+            return 1 + self.izq.largo() + self.der.largo()
+    
+    def altura(self):
+        if self.izq is None and self.der is None:
+            return 1
+        
+        elif self.izq is None:
+            return 1 + self.der.altura()
+
+        elif self.der is None:
+            return 1 + self.izq.altura()
+
+        else:
+            return 1 + max(self.izq.altura(), self.der.altura()) 
 
 
 class Abb:
@@ -36,26 +76,36 @@ class Abb:
         self.raiz.add(llave, valor)
     
     def buscar(self, llave):
-        pass
+        if self.raiz is None:
+            return None
+        return self.raiz.buscar(llave)
     
     def largo(self):
-        pass
+        if self.raiz is None:
+            return 0
+        return self.raiz.largo()
     
     def altura(self):
-        pass
+        if self.raiz is None:
+            return 0
+        return self.raiz.altura()
 
     def min(self):
         pass
 
 
-
-
 nombres = Abb()
-nombres.add('Maca')
-nombres.add('Kari')
-nombres.add('Ricardo')
-nombres.add('Pablo')
-nombres.add('Osvaldo')
-nombres.add('Rafa')
-nombres.add('Flavio')
-import pdb; pdb.set_trace()
+nombres.add('Maca', 'info de Maca')
+nombres.add('Kari', 'info de Kari')
+nombres.add('Ricardo', 'info de Ricardo')
+nombres.add('Pablo', 'info de Pablo')
+nombres.add('Osvaldo', 'info de Osvaldo')
+nombres.add('Rafa', 'info de Rafa')
+nombres.add('Flavio', 'info de Flavio')
+nombres.add('Matias', 'info de Matias')
+nombres.add('Arturo', 'info de Arturo')
+
+print(nombres.buscar('Osvaldo'))
+print('número de elementos', nombres.largo())
+print('altura del árbol', nombres.altura())
+#import pdb; pdb.set_trace()
